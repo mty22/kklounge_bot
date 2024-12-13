e47113918fa79c62672d67602c19d45ee9554de5
Matt Taylor <matt@idle.host>
2024-12-13 22:36:46 +0800

    Move to webhook model and translate in real-time

--- bot/bot.py
+++ bot/bot.py
@@ -2,14 +2,15 @@
 
 import os
 import aiohttp
+import requests
 import discord
+import json
+import time
 from discord.ext import tasks
 from dotenv import load_dotenv
 from modules.logging_setup import _Logging
 from modules.ollama_api import _Ollama
 from langdetect import detect
-
-# Set up logging
 
 # Load .env settings
 load_dotenv()
@@ -26,6 +27,7 @@ class KklBot(discord.Client):
         self.OLLAMA_URL = os.getenv("OLLAMA_URL")
         self.OLLAMA_TOKEN = os.getenv("OLLAMA_TOKEN")
         self.OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
+        self.DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 
         # Init the bot.
         intents = discord.Intents.default()
@@ -63,72 +65,147 @@ class KklBot(discord.Client):
         if message.author == self.user:
             return  # Ignore messages from the bot itself
 
-        self.logging.info(f"Message: {message.author} (User ID: {message.author.id}) Channel: {message.channel} (Channel ID: {message.channel.id}) Server: {message.guild.name} (Server ID: {message.guild.id})")
+        # Check the message length, ignore anything under 50 characters.
+        if len(message.content) < 50 or len(message.content) > 1900:
+            self.logging.info(f"Message (under 50 characters, ignoring) {message.author} (User ID: {message.author.id}) Channel: {message.channel} (Channel ID: {message.channel.id}) Server: {message.guild.name} (Server ID: {message.guild.id})")
+            return
+
+        # Ignore threads.
+        if isinstance(message.channel, discord.Thread):
+            self.logging.info(f"Message (in thread) {message.author} (User ID: {message.author.id}) Channel: {message.channel} (Channel ID: {message.channel.id}) Server: {message.guild.name} (Server ID: {message.guild.id})")
+            return
+        else:
+            self.logging.info(f"Message: {message.author} (User ID: {message.author.id}) Channel: {message.channel} (Channel ID: {message.channel.id}) Server: {message.guild.name} (Server ID: {message.guild.id})")
 
         # Detect the users native language based on the message content.
         try:
             detected_language = detect(message.content)
             if detected_language == "en":
                 users_language="English"
+                translate_into_language="Japanese"
             elif detected_language == "ja":
                 users_language="Japanese"
+                translate_into_language="English"
             else:
-                users_language="Unknown"
+                # Assume English.
+                users_language="English"
+                translate_into_language="Japanese"
         except Exception as e:
-            self.logging.warning(f"Error detecting language: {e}")
-            users_language="Unknown"
+            self.logging.warning(f"Error detecting language: {e} - we assume English")
+            users_language="English"
 
         # Prepare to send data to AI.
-        ai_system_message = """
-            You are a Discord bot, and your job is to review message content from users.
+        ai_api_call_start = time.time()
 
-            You understand both English and Japanese and should act as an intermediate real-time translator who summarises messages.
-
-            You must NOT translate any message into languages that you do not understand (this means you can only use English and Japanese).
-
-            Sometimes user messages will contain a mix of English and Japanese, so you should decide what the native language is based on which language the majority of the message is written in.
-
-            You should always perform the following tasks without fail:
-            1) Take note of the original message.
-            2) Provide a short summary of the original message in the user's native language.
-            3) Translate the original message into either English or Japanese (e.g., If the user's native language is English, translate the original message into Japanese. Or, if the user's native language is Japanese, translate the original message into English.).
-            4) Provide a short summary of the translated message in the respective language that it was translated into.
-            5) Take note of what language was translated into.
-            6) Personalise the message, always refer to the username when speaking about the user.
-
-            Anything outside these tasks, you MUST not act. You MUST NOT provide any other information to the user other than the requested tasks.
-
-            Respond with the following keys in valid JSON format:
-            - native_language
-            - original_message
-            - original_message_summary
-            - translated_message
-            - translated_message_summary
-
-            You are NOT permitted to use markdown or any formatting other than JSON in your response.
-
-            Ensure your responses are always trimmed to remove excessive whitespace outside of the JSON response.
-            """
-
-        ai_interactions = [
-            {
-                "role": "system",
-                "content": f"{ai_system_message}"
-            },
-            {
-                "role": "user",
-                "content": f"Username: {message.author} Original message: {message.content} Native language detected: {users_language}"
-            }
-        ]
-
-        # Send request to AI.
+        # Summarise the original message.
         try:
-            ai_response = _Ollama.ollama_query(self.OLLAMA_URL,self.OLLAMA_TOKEN,self.OLLAMA_MODEL,ai_interactions)
+            self.logging.info(f"Sending request to AI - Summarise original message")
+            original_message_summary = _Ollama.ollama_query(self.OLLAMA_URL,self.OLLAMA_TOKEN,self.OLLAMA_MODEL,[
+                { "role": "user",
+                 "content": f"Please summarise the following message. Do not provide anything else but the summary: {message.content}"
+                }
+                ])
         except Exception as e:
-            self.logging.error(f"Issue with AI: {e}")
+            self.logging.error(f"Issue with AI (summary): {e}")
+            return
+
+        # Translate the original message.
+        try:
+            self.logging.info(f"Sending request to AI - Translate original message (from {users_language} into {translate_into_language})")
+            translated_message = _Ollama.ollama_query(self.OLLAMA_URL,self.OLLAMA_TOKEN,self.OLLAMA_MODEL,[
+                { "role": "user",
+                 "content": f"Please translate this message from {users_language} into {translate_into_language}. Do not provide anything else but the summary: {message.content}"
+                }
+                ])
+        except Exception as e:
+            self.logging.error(f"Issue with AI (translation): {e}")
+            return
+
+        # Translate the summary.
+        try:
+            self.logging.info(f"Sending request to AI - Translate original message summary (from {users_language} into {translate_into_language})")
+            translated_message_summary = _Ollama.ollama_query(self.OLLAMA_URL,self.OLLAMA_TOKEN,self.OLLAMA_MODEL,[
+                { "role": "user",
+                 "content": f"Please translate this message from {users_language} into {translate_into_language}. Do not provide anything else but the summary: {translated_message}"
+                }
+                ])
+        except Exception as e:
+            self.logging.error(f"Issue with AI (translation summary): {e}")
+            return
 
         # Log result.
-        self.logging.info(f"AI response: {ai_response}")
+        ai_api_call_duration = time.time() - ai_api_call_start
+        self.logging.info(f"AI response (duration: {ai_api_call_duration} seconds)")
+
+        # Convert the repsonse to a dictionary.
+        discord_message_link = f"https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}"
+
+        # Prepare Discord webhook embed.
+        webhook_payload = {
+                "username": "KKL Bot",
+                "avatar_url": "https://cdn.discordapp.com/avatars/1316008262121816094/7c772c0978df400f722b0a2f119e5142.png",
+                "content": f"Message author / メッセージ作成者: `{message.author}` Channel / チャネル: `{message.channel}`",
+                "embeds": [
+                    {
+                        "title": "Original message / 元のメッセージ",
+                        "description": f"{message.content}",
+                        "color": 255,
+                        "fields": [
+                            {
+                                "name": "Summary",
+                                "value": f"{original_message_summary}",
+                                "inline": False
+                            }
+                        ]
+                    },
+                    {
+                        "title": "Translated message",
+                        "description": f"{translated_message}",
+                        "color": 65280,
+                        "fields": [
+                            {
+                                "name": "Message summary / メッセージの概要",
+                                "value": f"{translated_message_summary}",
+                                "inline": False
+                            }
+                        ]
+                    },
+                    {
+                        "title": "Other",
+                        "color": 16753920,
+                        "fields": [
+                            {
+                                "name": "AI model / AIモデル",
+                                "value": f"`{self.OLLAMA_MODEL}`",
+                                "inline": True
+                            },
+                            {
+                                "name": "Generation time / 生成時間",
+                                "value": f"`{ai_api_call_duration:.2f} seconds`",
+                                "inline": True
+                            },
+                            {
+                                "name": "Original Message Link / 元のメッセージリンク",
+                                "value": f"[View message / メッセージを見る]({discord_message_link})",
+                                "inline": False
+                            }
+                        ]
+                    }
+                ]
+            }
+
+        # Send the message to Discord.
+        try:
+            webhook_response = requests.post(self.DISCORD_WEBHOOK_URL, json=webhook_payload)
+        except Exception as e:
+            self.logging.error(f"Webhook POST error: {e}")
+            return
+        finally:
+            if webhook_response.status_code == 204:
+                self.logging.info(f"Webhook sent successfully about user {message.author} in {message.guild.name}")
+            else:
+                self.logging.error(f"Failed to send webhook. Status: {webhook_response.status_code}, Reason: {webhook_response.text}")
+
 
 # Create the bot instance and run it.
 bot = KklBot()
